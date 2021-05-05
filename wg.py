2b3f50ed149c27aa8099bbe9556ae464cb51764b
KINGSTEVE005421 <78914977+KINGSTEVE005421@users.noreply.github.com>
2021-05-05 21:25:12 +0530

    Add files via upload

---
+++ wg.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.system('C:\Windows\system32>"C:\Program Files\WireGuard\wireguard.exe" /installtunnelservice , "C:\Program Files\WireGuard\Data\Configurations\wireguard1.conf.dpapi"')
