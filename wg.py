447d08fabc8524fdab2620ceda5ca204a0b8f313
KINGSTEVE005421 <78914977+KINGSTEVE005421@users.noreply.github.com>
2021-05-05 21:15:10 +0530

    Delete wg.py

--- wg.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-os.system('C:\Windows\system32>"C:\Program Files\WireGuard\wireguard.exe" /installtunnelservice , "C:\Program Files\WireGuard\Data\Configurations\wireguard1.conf.dpapi"')
