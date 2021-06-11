8e64ef00504a95011d50a7547eb06fbf5d3906bb
KINGSTEVE005421 <canva4994@gmail.com>
2021-06-11 20:41:56 +0530

    jarvis protocol enabled

--- main.py
+++ main.py
@@ -12,11 +12,11 @@
 
 import sys
 import platform
-#from PyQt5.QtCore import *
+# from PyQt5.QtCore import *
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect,
                             QSize, QTime, QUrl, Qt, QEvent)
-#from PyQt5.QtWidgets import QApplication
+# from PyQt5.QtWidgets import QApplication
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence,
                            QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
