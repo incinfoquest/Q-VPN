0745b9f28660b1a4a109111a015ab15a1cb60d65
andthens <andanandans2002@gmail.com>
2021-06-26 21:30:42 +0530

    Update pop.py

--- pop.py
+++ pop.py
@@ -11,7 +11,7 @@ class MainWindow(QMainWindow):
         self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
         self.resize(600, 500)
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('https://google.com/'))
+        self.browser.setUrl(QUrl('http://172.41.31.254/admin'))
         self.setCentralWidget(self.browser)
         self.setWindowIcon(QtGui.QIcon('ad.png'))
         self.show()
